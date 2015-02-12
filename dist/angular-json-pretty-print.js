9af225791271def34b93048646f0a30f5fdf3b0b
Rodrigo Morais <morais.rm@gmail.com>
2015-02-12 09:09:04 -0200

    Creating a include template to build HTML recursively.

--- dist/angular-json-pretty-print.js
+++ dist/angular-json-pretty-print.js
@@ -4,11 +4,15 @@ angular.module('JsonPrettyPrint').run(['$templateCache', function($templateCache
 
   $templateCache.put('component/templates/jsonPrettyPrint.html',
     "<code>\n" +
-    "    <div class=\"json-treeview\" data-ng:repeat=\"line in jsonPretty\">\n" +
-    "        <i data-ng:repeat=\"object in line.elements\" class=\"fa fa-minus-square-o {{object.class}}\" id=\"{{object.id}}\" class=\"fa fa-minus-square-o plus-icon\" data-ng:if=\"object.isPlusIcon\"></i>\n" +
-    "        <span data-ng:repeat=\"object in line.elements\" class=\"{{object.class}}\" data-ng:if=\"object.isPlusIcon == false\">{{object.element}}</span>\n" +
+    "    <div class=\"json-treeview\" data-ng:repeat=\"line in jsonPretty\" data-ng:include=\"'component/templates/line.html'\">\n" +
     "    </div>\n" +
     "</code>"
+  );
+
+
+  $templateCache.put('component/templates/line.html',
+    "<i data-ng:repeat=\"object in line.elements\" class=\"fa fa-minus-square-o {{object.class}}\" id=\"{{object.id}}\" class=\"fa fa-minus-square-o plus-icon\" data-ng:if=\"object.isPlusIcon\"></i>\n" +
+    "<span data-ng:repeat=\"object in line.elements\" class=\"{{object.class}}\" data-ng:if=\"object.isPlusIcon == false\">{{object.element}}</span>"
   );
 
 }]);
