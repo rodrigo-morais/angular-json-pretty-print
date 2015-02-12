b9fd2130f9cb4c7206d079893d78533db6b93b9b
Rodrigo Morais <morais.rm@gmail.com>
2015-02-12 08:23:26 -0200

    Adjusting the directive file to do not have the leak of variables, functions and etc to context.

--- component/directives/angular-json-pretty-print-directive.js
+++ component/directives/angular-json-pretty-print-directive.js
@@ -1,78 +1,80 @@
-jsonPrettyPrint.directive('rmJsonPrettyPrint',
-[rmJsonPrettyPrintDirective]);
+(function() {
+    jsonPrettyPrint.directive('rmJsonPrettyPrint',
+    [rmJsonPrettyPrintDirective]);
 
-function rmJsonPrettyPrintDirective() {
+    function rmJsonPrettyPrintDirective() {
 
-    var _createObject = function(){
-        var jsonLines = [],
+        var _createObject = function(){
+            var jsonLines = [],
+                jsonLine = {
+                    elements: [],
+                    lines: []
+                },
+                jsonObject = {},
+                plusId = 0;
+
+            jsonObject.id = 'plus_' + plusId;
+            jsonObject.isPlusIcon = true;
+            jsonObject.isBlank = false;
+            jsonObject.element = '';
+            jsonObject.style = '';
+            jsonObject.class = 'plus-icon';
+            jsonLine.elements.push(jsonObject);
+            
+            jsonObject = {};
+            jsonObject.id = '';
+            jsonObject.isPlusIcon = false;
+            jsonObject.isBlank = false;
+            jsonObject.element = '{';
+            jsonObject.style = '';
+            jsonObject.class = 'json-brace';
+            jsonLine.elements.push(jsonObject);
+            jsonLines.push(jsonLine);
+
             jsonLine = {
                 elements: [],
                 lines: []
-            },
-            jsonObject = {},
-            plusId = 0;
+            };
+            jsonObject = {};
+            jsonObject.id = '';
+            jsonObject.isPlusIcon = false;
+            jsonObject.isBlank = false;
+            jsonObject.element = '}';
+            jsonObject.style = '';
+            jsonObject.class = 'json-brace';
+            jsonLine.elements.push(jsonObject);
+            jsonLines.push(jsonLine);        
 
-        jsonObject.id = 'plus_' + plusId;
-        jsonObject.isPlusIcon = true;
-        jsonObject.isBlank = false;
-        jsonObject.element = '';
-        jsonObject.style = '';
-        jsonObject.class = 'plus-icon';
-        jsonLine.elements.push(jsonObject);
-        
-        jsonObject = {};
-        jsonObject.id = '';
-        jsonObject.isPlusIcon = false;
-        jsonObject.isBlank = false;
-        jsonObject.element = '{';
-        jsonObject.style = '';
-        jsonObject.class = 'json-brace';
-        jsonLine.elements.push(jsonObject);
-        jsonLines.push(jsonLine);
-
-        jsonLine = {
-            elements: [],
-            lines: []
+            return jsonLines;
         };
-        jsonObject = {};
-        jsonObject.id = '';
-        jsonObject.isPlusIcon = false;
-        jsonObject.isBlank = false;
-        jsonObject.element = '}';
-        jsonObject.style = '';
-        jsonObject.class = 'json-brace';
-        jsonLine.elements.push(jsonObject);
-        jsonLines.push(jsonLine);        
 
-        return jsonLines;
-    };
+        var _prettifyJson = function(json){
+            var jsonObject = JSON.parse(json),
+                jsonLines = [];
 
-    var _prettifyJson = function(json){
-        var jsonObject = JSON.parse(json),
-            jsonLines = [];
+            if(Array.isArray(jsonObject)){
 
-        if(Array.isArray(jsonObject)){
+            }
+            else{
+                var _jsonLines = _createObject(json);
+                jsonLines = jsonLines.concat(_jsonLines);
+            }
 
-        }
-        else{
-            var _jsonLines = _createObject(json);
-            jsonLines = jsonLines.concat(_jsonLines);
-        }
+            return jsonLines;
+        };
 
-        return jsonLines;
-    };
+        var html = 'component/templates/jsonPrettyPrint.html';
 
-    var html = 'component/templates/jsonPrettyPrint.html';
+        return {
+            restrict: 'E',
+            templateUrl: html,
+            replace: true,
+            link: function (scope, element, attrs, controller) {
+                attrs.$observe("json", function (newValue) {
+                    scope.jsonPretty = _prettifyJson(newValue);
+                });
+            }
+        };
 
-    return {
-        restrict: 'E',
-        templateUrl: html,
-        replace: true,
-        link: function (scope, element, attrs, controller) {
-            attrs.$observe("json", function (newValue) {
-                scope.jsonPretty = _prettifyJson(newValue);
-            });
-        }
-    };
-
-}
+    }
+})();
